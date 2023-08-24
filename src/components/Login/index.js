ccfd73dbd4af878acc733162c493fa0af53cb9bc
shashank <shashankmende88@gmail.com>
2023-08-24 10:40:15 +0000

    Second commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,1 +1,14 @@
 // Write your code here
+import { Component } from 'react'
+import './index.css'
+
+class Login extends Component {
+    render() {
+        return (
+            <h1>Please Login</h1>
+            <button>Login</button>
+        )
+    }
+}
+
+export default Login
